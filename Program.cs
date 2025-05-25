b6bd34b9a536e141eb6a7537157b911c55968ef4
Cesar Orellana <cesarlorellana184@gmail.com>
2025-05-24 22:27:01 -0300

    Ejercicio2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// Ejercicio 1
+
 int a;
 int b;
 a = 10;
@@ -8,3 +10,18 @@ b = a;
 
 Console.WriteLine("Valor de a:" + a);
 Console.WriteLine("Valor de b:" + b);
+
+// Ejercicio 2
+
+string num = "120";
+int i = 0;
+bool result = int.TryParse(num, out i);
+Console.Write(i);
+Console.WriteLine("");
+int aux = 0;
+while (i > 0)
+{
+    aux = i % 10;
+    i = i / 10;
+    Console.Write(aux);
+}
