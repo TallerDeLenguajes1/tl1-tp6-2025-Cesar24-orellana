a855ffa9ec45da4b5638d4c8239b37077c6f2653
Cesar Orellana <cesarlorellana184@gmail.com>
2025-05-24 23:21:00 -0300

    ahora-si-ejercicio2

--- Program.cs
+++ Program.cs
@@ -11,8 +11,6 @@ b = a;
 Console.WriteLine("Valor de a:" + a);
 Console.WriteLine("Valor de b:" + b);
 
-// Ejercicio 2
-
 string num = "120";
 int i = 0;
 bool result = int.TryParse(num, out i);
@@ -24,4 +22,83 @@ while (i > 0)
     aux = i % 10;
     i = i / 10;
     Console.Write(aux);
+}
+
+
+// Ejercicio 2
+
+int num1 = 0;
+int num2 = 0;
+int resultado = 0;
+int opcion = 0, repetir = 1;
+float resultadoReales = 0;
+do
+{
+    Console.WriteLine("\n1. Suma\n2. Resta \n3.Division\n4. Multiplicacion\n0. Salir");
+    opcion = Console.Read();
+    switch (opcion)
+    {
+        case 1:
+            resultado = FuncionSuma(num1, num2);
+            Mensaje(resultado);
+            break;
+        case 2:
+            resultado = FuncionResta(num1, num2);
+            Mensaje(resultado);
+            break;
+        case 3:
+            resultadoReales = FuncionDivision(num1, num2);
+            Mensaje2(resultadoReales);
+            break;
+        case 4:
+            resultado = FuncionMultiplicacion(num1, num2);
+            Mensaje(resultado);
+            break;
+    }
+    Console.WriteLine("Desea Continuar?\n1. Si\0. No");
+    repetir = Console.Read();
+} while (repetir == 1);
+
+int FuncionSuma(int num1, int num2)
+{
+    Console.WriteLine("Ingrese un Numero: ");
+    num1 = Console.Read();
+    Console.WriteLine("Ingrese un 2do Numero: ");
+    num2 = Console.Read();
+    return num1 + num2;
+}
+
+int FuncionResta(int num1, int num2)
+{
+    Console.WriteLine("Ingrese un Numero: ");
+    num1 = Console.Read();
+    Console.WriteLine("Ingrese un 2do Numero: ");
+    num2 = Console.Read();
+    return num1 - num2;
+}
+
+float FuncionDivision(int num1, int num2)
+{
+    Console.WriteLine("Ingrese un Numero: ");
+    num1 = Console.Read();
+    Console.WriteLine("Ingrese un 2do Numero: ");
+    num2 = Console.Read();
+    return num1 / num2;
+}
+
+int FuncionMultiplicacion(int num1, int num2)
+{
+    Console.WriteLine("Ingrese un Numero: ");
+    num1 = Console.Read();
+    Console.WriteLine("Ingrese un 2do Numero: ");
+    num2 = Console.Read();
+    return num1 * num2;
+}
+
+void Mensaje(int resultado) {
+    Console.WriteLine("Su resultado es: " + resultado);
+}
+
+void Mensaje2(float resultado) {
+    Console.WriteLine("Su resultado es: " + resultado);
 }
