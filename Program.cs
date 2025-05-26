5fad500a846949631c4913874bb518fa3e2a3591
Cesar Orellana <cesarlorellana184@gmail.com>
2025-05-25 21:05:01 -0300

    CalculadoraV2-con-errores

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 int a;
@@ -8,3 +9,69 @@ b = a;
 
 Console.WriteLine("Valor de a:" + a);
 Console.WriteLine("Valor de b:" + b);
+
+// Ejercicio 3
+
+float num = 0;
+
+Console.WriteLine("Ingrese un numero: ");
+num = Console.Read();
+
+ValorAbsoluto(num);
+Cuadrado(num);
+RaizCuadrada(num);
+SenoYCoseno(num);
+RedondearNumero(num);
+
+int numA = 0;
+int numB = 0;
+
+Console.WriteLine("Ingrese un numero: ");
+numA = Console.Read();
+Console.WriteLine("Ingrese un 2do numero: ");
+numB = Console.Read();
+
+CompararNumeros(numA, numB);
+
+// Funciones
+
+void ValorAbsoluto(float num)
+{
+    double resultado;
+    resultado = Math.Abs(num);
+    Console.WriteLine("\nEl Valor Absoluto de su numero es: " + resultado);
+}
+
+void Cuadrado(float num)
+{
+    double resultado;
+    resultado = Math.Pow(num, 2);
+    Console.WriteLine("\nEl Cuadrado de su numero es: " + resultado);
+}
+
+void RaizCuadrada(float num)
+{
+    double resultado = Math.Sqrt(num);
+    Console.WriteLine("\nLa raiz de su numero es: " + resultado);
+}
+
+void SenoYCoseno(float num)
+{
+    double resultado1 = Math.Sin(num);
+    double resultado2 = Math.Cos(num);
+    Console.WriteLine("\nEl Seno de su numero es: " + resultado1);
+    Console.WriteLine("\nEl Coseno de su numero es: " + resultado2);
+}
+
+void RedondearNumero(float num)
+{
+    double resultado = Math.Round(num);
+}
+
+void CompararNumeros(int numA,int numB)
+{
+    int max = Math.Max(numA, numB);
+    int min = Math.Min(numA, numB);
+    Console.WriteLine("\nNumero Maximo es: " + max);
+    Console.WriteLine("\nNumero Minimo es: " + min);
+}
